4d940b2df078c5ed1879633210ed79253457c3f2
arxidamos <dimos33@hotmail.com>
2023-04-02 16:28:30 +0300

    Added method to ask for xlsx file from backend

--- src/components/Pages/Export.js
+++ src/components/Pages/Export.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 import * as XLSX from 'xlsx';
+import './Export.css'
+// import existingExcel from '/home/dimos/Desktop/ectsTool/ects-tool/src/Οδηγός για την λήψη πτυχίου.xls'
 
-function convertToExcel(courses) {
+const convertToExcel = (courses) => {
   const workbook = XLSX.utils.book_new();
   const sheet = XLSX.utils.json_to_sheet(courses);
   XLSX.utils.book_append_sheet(workbook, sheet, 'Sheet1');
   return workbook;
 }
 
-function downloadAsExcel(workbook, filename) {
+const downloadAsExcel = (workbook, filename) => {
   // Generate binary data for excel file
   const wbout = XLSX.write(workbook, { bookType: 'xlsx', type: 'binary' });
   
@@ -44,7 +46,41 @@ function downloadAsExcel(workbook, filename) {
   }
 }
 
-function downloadAsJson(data, filename) {
+const sendCoursesReceiveExcel = async (data, filename) => {
+  try {
+    const response = await fetch('http://localhost:5000/api/updateExcel', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    // const responseData = await response.json();
+    // return responseData;
+    if (!response.ok) {
+      throw new Error('Response from server not ok');
+    }
+
+    // Get xlsx from response
+    const blob = await response.blob();
+
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(() => {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 0);
+  }
+  catch (error) {
+    console.error(error);
+  }
+};
+
+const downloadAsJson = (data, filename) => {
   const jsonData = JSON.stringify(data);
   const blob = new Blob([jsonData], { type: 'application/json' });
   const url = URL.createObjectURL(blob);
@@ -59,16 +95,47 @@ function downloadAsJson(data, filename) {
   }, 0);
 }
 
+// const readExcelFile = (courses) => {
+//   const file = require('/home/dimos/Desktop/ectsTool/ects-tool/src/Οδηγός για την λήψη πτυχίου.xls');
+//   const workbook = XLSX.readFile(file);
+//   const sheetName = 'Sheet1'; // replace with your sheet name
+//   const sheet = workbook.Sheets[sheetName];
+
+//   // Find the column index of column A
+//   const columnAIndex = XLSX.utils.decode_col('C');
+
+//   // Find the column index of column C
+//   const columnCIndex = XLSX.utils.decode_col('E');
+
+//   courses.forEach((item, rowIndex) => {
+//     const cellAddress = XLSX.utils.encode_cell({
+//       r: rowIndex + 1, // add 1 because row indexing starts from 1
+//       c: columnAIndex,
+//     });
+//     const cell = sheet[cellAddress];
+//     if (cell && cell.v.includes(item.course)) {
+//       const cellCAddress = XLSX.utils.encode_cell({
+//         r: rowIndex + 1,
+//         c: columnCIndex,
+//       });
+//       XLSX.utils.sheet_set_cell(sheet, cellCAddress, item.grade);
+//     }
+//   });
+
+//   XLSX.writeFile(workbook, 'excel_me_vathmous.xlsx');
+// }
+
 const Export = ({ coursesData }) => {
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
-    const translatedData = coursesData.map(item => ({
-      ΜΑΘΗΜΑ: item.course,
-      ECTS: item.ects,
-      ΒΑΘΜΟΣ: item.grade,
-    }));
-    setCourses(translatedData);
+    // const translatedData = coursesData.map(item => ({
+    //   ΜΑΘΗΜΑ: item.course,
+    //   ECTS: item.ects,
+    //   ΒΑΘΜΟΣ: item.grade,
+    // }));
+    // setCourses(translatedData);
+    setCourses(coursesData);
   }, [coursesData])
 
 
@@ -81,17 +148,86 @@ const Export = ({ coursesData }) => {
     downloadAsJson(courses, 'data.json');
   };
 
+  const handleSpecialDownload = async () => {
+    // readExcelFile(courses);
+    try {
+      await sendCoursesReceiveExcel(courses, 'Οδηγός για τη λήψη πτυχίου.xlsx');
+    }
+    catch (error) {
+      console.error(error);
+    }
+  };
+
+ 
+
   return (
     <div>
-      <h1>Εξαγωγή βαθμολογίας σε Excel</h1>
-      <button onClick={handleExcelDownload}>
-        Κατέβασμα Excel
-      </button>
-      <button onClick={handleJsonDownload}>
-        Κατέβασμα JSON
-      </button>
+      <h2>Εξαγωγή βαθμολογίας</h2>
+      <div className="button-area">
+        <button onClick={handleExcelDownload} className="chunky-button">
+          Κατέβασμα Excel
+        </button>
+        <button onClick={handleJsonDownload} className="chunky-button">
+          Κατέβασμα JSON
+        </button>
+        <button onClick={handleSpecialDownload} className="chunky-button">
+          Κατέβασμα sosovaro
+        </button>
+      </div>
     </div>
   );
 }
 
 export default Export;
+
+
+
+// const workbook = XLSX.readFile('myWorkbook.xlsx');
+// const sheetName = 'Sheet1'; // replace with your sheet name
+// const sheet = workbook.Sheets[sheetName];
+
+// const data = [
+//   { attr1: 'value1', attr2: 'value2' },
+//   { attr1: 'value3', attr2: 'value4' },
+// ];
+
+// // Find the column index of column A
+// const columnAIndex = XLSX.utils.decode_col('A');
+
+// // Find the column index of column C
+// const columnCIndex = XLSX.utils.decode_col('C');
+
+// data.forEach((obj, rowIndex) => {
+//   const cellAddress = XLSX.utils.encode_cell({
+//     r: rowIndex + 1, // add 1 because row indexing starts from 1
+//     c: columnAIndex,
+//   });
+//   const cell = sheet[cellAddress];
+//   if (cell && cell.v === obj.attr1) {
+//     const cellCAddress = XLSX.utils.encode_cell({
+//       r: rowIndex + 1,
+//       c: columnCIndex,
+//     });
+//     XLSX.utils.sheet_set_cell(sheet, cellCAddress, obj.newValue);
+//   }
+// });
+
+// XLSX.writeFile(workbook, 'myWorkbookModified.xlsx');
+
+
+// const postData = async (data) => {
+//   try {
+//     const response = await fetch('/api/updateExcel', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(data)
+//     });
+//     const responseData = await response.json();
+//     return responseData;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
